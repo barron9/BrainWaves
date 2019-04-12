0cec046f01ce21a3fde8dfd9ff7efe34d553e476
Teon L Brooks <teon.brooks@gmail.com>
2019-04-11 23:20:06 -0400

    overhaul continues

--- app/utils/filesystem/storage.ts
+++ app/utils/filesystem/storage.ts
@@ -75,7 +75,7 @@ export const storeBehaviouralData = (
 };
 
 // Stores an image to workspace dir
-export const storeJupyterImage = (
+export const storePyodideImage = (
   title: string,
   imageTitle: string,
   rawData: Buffer
